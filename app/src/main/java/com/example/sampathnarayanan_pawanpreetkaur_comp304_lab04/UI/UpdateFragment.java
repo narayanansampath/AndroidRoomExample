43334d99e43b82ce5124337c35cca20030b87dec
Sampath Narayanan <sampathnarayanan72@gmail.com>
2020-08-02 20:46:28 -0400

    Minor improvements

--- app/src/main/java/com/example/sampathnarayanan_pawanpreetkaur_comp304_lab04/UI/UpdateFragment.java
+++ app/src/main/java/com/example/sampathnarayanan_pawanpreetkaur_comp304_lab04/UI/UpdateFragment.java
@@ -13,22 +13,31 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+import com.example.sampathnarayanan_pawanpreetkaur_comp304_lab04.Database.Patient;
 import com.example.sampathnarayanan_pawanpreetkaur_comp304_lab04.R;
+import com.example.sampathnarayanan_pawanpreetkaur_comp304_lab04.ViewModel.PatientViewModel;
+
+import java.util.List;
 
 
 public class UpdateFragment extends Fragment {
 
     EditText etPatientId;
     Button viewInfo, updateInfo;
+    PatientViewModel viewModel;
+    boolean noMatchFound = true;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         View rootView =  inflater.inflate(R.layout.fragment_update, container, false);
-         etPatientId = rootView.findViewById(R.id.updatet1);
-         viewInfo =  rootView.findViewById(R.id.updatetb1);
+        View rootView =  inflater.inflate(R.layout.fragment_update, container, false);
+        etPatientId = rootView.findViewById(R.id.updatet1);
+        viewInfo =  rootView.findViewById(R.id.updatetb1);
         updateInfo =  rootView.findViewById(R.id.updateb2);
+        viewModel =  ViewModelProviders.of(this).get(PatientViewModel.class);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Update Patient info");  // provide compatibility to all the versions
 
@@ -39,10 +48,15 @@ public class UpdateFragment extends Fragment {
                     Toast.makeText(getActivity(), "Invalid patient id please enter a valid patient id.", Toast.LENGTH_SHORT).show();
 
                 } else {
-                    Intent intent = new Intent(getActivity(), UpdatePatientActivity.class);
-                    intent.putExtra("patientId", Integer.valueOf(etPatientId.getText().toString()));
-                    intent.putExtra("isEditable", false);
-                    startActivity(intent);
+                    checkPatientId( Integer.valueOf(etPatientId.getText().toString()));
+                    if(noMatchFound){
+                        Toast.makeText(getActivity(), "No patient data found.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent intent = new Intent(getActivity(), UpdatePatientActivity.class);
+                        intent.putExtra("patientId", Integer.valueOf(etPatientId.getText().toString()));
+                        intent.putExtra("isEditable", false);
+                        startActivity(intent);
+                    }
                 }
             }
         });
@@ -54,15 +68,33 @@ public class UpdateFragment extends Fragment {
                     Toast.makeText(getActivity(), "Invalid patient id please enter a valid patient id.", Toast.LENGTH_SHORT).show();
 
                 } else {
-                    Intent intent = new Intent(getActivity(), UpdatePatientActivity.class);
-                    intent.putExtra("patientId", Integer.valueOf(etPatientId.getText().toString()));
-                    intent.putExtra("isEditable", false);
-                    intent.putExtra("isEditable", true);
-                    startActivity(intent);
+                    checkPatientId( Integer.valueOf(etPatientId.getText().toString()));
+                    if(noMatchFound){
+                        Toast.makeText(getActivity(), "No patient data found.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent intent = new Intent(getActivity(), UpdatePatientActivity.class);
+                        intent.putExtra("patientId", Integer.valueOf(etPatientId.getText().toString()));
+                        intent.putExtra("isEditable", false);
+                        intent.putExtra("isEditable", true);
+                        startActivity(intent);
+                    }
                 }
             }
         });
 
-         return rootView;
+        return rootView;
+    }
+
+    private void checkPatientId(final int patientId) {
+        viewModel.getAllPatient().observe(getViewLifecycleOwner(), new Observer<List<Patient>>() {
+            @Override
+            public void onChanged(List<Patient> patients) {
+                for(Patient data : patients) {
+                    if(data.getPatientId() == patientId){
+                        noMatchFound = false;
+                    }
+                }
+            }
+        });
     }
 }
